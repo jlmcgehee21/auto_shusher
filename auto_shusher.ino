861aea4f28ac22c195e5d9a337f64017282ada78
Jeff McGehee <jlmcgehee21@gmail.com>
2017-06-29 08:28:54 -0400

    initial working auto_shusher

--- auto_shusher.ino
+++ auto_shusher.ino
@@ -1,16 +1,46 @@
 #include <Keyboard.h>
 
 char to_press = KEY_F1;
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   Keyboard.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(10000);
-  Keyboard.press(to_press);
-  delay(1000);
-  Keyboard.release(to_press);
+  unsigned long startMillis= millis();  // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+
+  // collect data for 50 mS
+  while (millis() - startMillis < sampleWindow)
+  {
+    sample = analogRead(0);
+    if (sample < 1024)  // toss out spurious readings
+    {
+       if (sample > signalMax)
+       {
+          signalMax = sample;  // save just the max levels
+       }
+       else if (sample < signalMin)
+       {
+          signalMin = sample;  // save just the min levels
+       }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+  double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+
+  if ( volts > 1 ) {
+    Keyboard.press(to_press);
+  }
+  else {
+    Keyboard.release(to_press);
+  }
 }
